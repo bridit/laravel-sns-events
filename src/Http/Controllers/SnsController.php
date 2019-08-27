401f06ff4a6cdcf7ab878f620397ba4bea6ba016
Nicklas Kevin Frank <nicklas@monori.dk>
2019-08-27 15:38:29 +0200

    Initial commit

---
+++ src/Http/Controllers/SnsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AllerMedia\LaravelSnsEvents\Http\Controllers;
+
+use Aws\Sns\Message;
+use Aws\Sns\MessageValidator;
+use Illuminate\Routing\Controller;
+use AllerMedia\LaravelSnsEvents\Events\SnsEvent;
+use AllerMedia\LaravelSnsEvents\Events\SnsSubscriptionConfirmation;
+
+class SnsController extends Controller
+{
+    public function handle()
+    {
+        // Instantiate the Message and Validator
+        $message = Message::fromRawPostData();
+        $validator = new MessageValidator();
+
+        // Validate the message.
+        try {
+            $validator->validate($message);
+        } catch (InvalidSnsMessageException $e) {
+            // Return 404 to pretend we are not here for SNS if invalid request
+            return response('SNS Message Validation Error: ' . $e->getMessage(), 404);
+        }
+
+        // Check the type of the message and handle the subscription.
+        if ($message['Type'] === 'SubscriptionConfirmation') {
+            // Confirm the subscription by sending a GET request to the SubscribeURL
+            file_get_contents($message['SubscribeURL']);
+            event(new SnsSubscriptionConfirmation);
+            return response('OK', 200);
+        }
+
+        if ($message['Type'] === 'Notification') {
+            event(new SnsEvent($message));
+        }
+
+        return response('OK', 200);
+    }
+}
